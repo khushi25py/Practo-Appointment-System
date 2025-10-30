8adc1b393e912ecc0739a3e17de5fa19014bf5ed
khushi25py <guptakhushi2514@gmail.com>
2025-10-30 23:42:23 +0530

    Fixed static files with WhiteNoise

--- mypracto/settings.py
+++ mypracto/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,"static")]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
@@ -138,3 +139,5 @@ EMAIL_HOST_USER = 'guptakhushi2514@gmail.com' # sender-mail ID
 EMAIL_HOST_PASSWORD = 'nofnikjsrzeohtln' # in-app password
 
 APPEND_SLASH = False
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
